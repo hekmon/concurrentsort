c21f1832133e34b4814ee7e97fa9aa1931cf8da8
Edouard Hur <edouardhur@gmail.com>
2017-07-06 12:55:11 +0200

    quicksort.bench - remove unecessary code

--- quicksort.bench/main.go
+++ quicksort.bench/main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"concurrentsort"
-)
-
 func main() {
-	best := QuickSortMinSizeCompute(0, 2, 20, 1<<24, 1<<7, 4)
-	concurrentsort.QuickSortMinSizeForConcurrency = best
+	QuickSortMinSizeCompute(0, 2, 20, 1<<24, 1<<7, 4)
 }
